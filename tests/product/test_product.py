43b03906c90eb4aaa6f57a4b289d8079484c38e7
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-11 20:17:47 -0300

    tests - req 01 - tests the constructor/initializer of the Product object

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    storaging = "Local seco e arejado ao abrigo do sol"
+
+    product = Product(
+        1,
+        "Toddynho",
+        "Toddy",
+        "07/03/2021",
+        "01/05/2024",
+        "TDNH-1234",
+        "Local seco e arejado ao abrigo do sol",
+        )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Toddynho"
+    assert product.nome_da_empresa == "Toddy"
+    assert product.data_de_fabricacao == "07/03/2021"
+    assert product.data_de_validade == "01/05/2024"
+    assert product.numero_de_serie == "TDNH-1234"
+    assert product.instrucoes_de_armazenamento == storaging
