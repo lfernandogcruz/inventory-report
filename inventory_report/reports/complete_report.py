866dcf3fb00d56eed9c704799b39b7e955232eaf
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-11 23:17:56 -0300

    feat: req 03 - generates a complete version of the report using the simple one that was implemented beforehand

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+
+    @staticmethod
+    def generate(prod_list):
+        simple_report = SimpleReport.generate(prod_list)
+
+        companies_counter = Counter(
+            [product["nome_da_empresa"] for product in prod_list]
+        ).items()
+
+        result = f"{simple_report}\n"
+        result += "Produtos estocados por empresa:\n"
+        for company, count in companies_counter:
+            result += f"- {company}: {count}\n"
+
+        return result
