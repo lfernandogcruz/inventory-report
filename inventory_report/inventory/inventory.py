1ef16f611312c2f2814a161eee34763c2ac60f57
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-17 19:59:20 -0300

    feat: req 06 - adds method to handle XML files to generate reports

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import json
+import xmltodict
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -16,9 +17,13 @@ class Inventory:
                 reader = csv.DictReader(file)
                 info = [row for row in reader]
 
-            if extension == ".json":
+            elif extension == ".json":
                 reader = file.read()
                 info = json.loads(reader)
+
+            else:
+                reader = xmltodict.parse(file.read())
+                info = reader["dataset"]["record"]
 
         if report_type == "simples":
             generated_report = SimpleReport.generate(info)
@@ -32,3 +37,10 @@ class Inventory:
 # https://pt.stackoverflow.com/questions/382049/verificar-extens%C3%B5es-de-arquivos-em-python
 # and at:
 # https://horadecodar.com.br/2021/04/17/extrair-extensao-do-arquivo-com-python/
+#
+# after talking to some coleagues, they suggested me to use
+# the XMLTODICT library to handle the XML file, so I did some research
+# and found these to help me go through it:
+# https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
+# https://www.digitalocean.com/community/tutorials/python-xml-to-json-dict
+# https://www.askpython.com/python-modules/xmltodict-module
