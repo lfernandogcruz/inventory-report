12b5f0d359369e4ffd20cda17fa8c3b2daedd2e3
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-17 19:03:27 -0300

    feat: req 05 - adds a method to implement a JSON based report

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,7 @@
+import os
 import csv
+import json
+
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,13 +10,25 @@ class Inventory:
     @staticmethod
     def import_data(path, report_type):
         with open(path, "r") as file:
-            reader = csv.DictReader(file)
-            info = [row for row in reader]
+            extension = os.path.splitext(path)[1]
 
-            if report_type == "simples":
-                generated_report = SimpleReport.generate(info)
-            elif report_type == "completo":
-                generated_report = CompleteReport.generate(info)
-            else:
-                raise ValueError("Tipo de relatório inválido!")
-            return generated_report
+            if extension == ".csv":
+                reader = csv.DictReader(file)
+                info = [row for row in reader]
+
+            if extension == ".json":
+                reader = file.read()
+                info = json.loads(reader)
+
+        if report_type == "simples":
+            generated_report = SimpleReport.generate(info)
+        elif report_type == "completo":
+            generated_report = CompleteReport.generate(info)
+        else:
+            raise ValueError("Tipo de relatório inválido.")
+        return generated_report
+
+# research on how to extract the file extension from a path made at:
+# https://pt.stackoverflow.com/questions/382049/verificar-extens%C3%B5es-de-arquivos-em-python
+# and at:
+# https://horadecodar.com.br/2021/04/17/extrair-extensao-do-arquivo-com-python/
