da5b468bfc82d644a2a4d8eb984f4a22bc6a3382
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-17 20:25:13 -0300

    test: req 08 - creates simple test to Product Report functionality

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    instructions = "Don't know what I want, but I know how to get it"
+    mock = Product(
+        77,
+        "Never Mind The Bollocks",
+        "S Pistols",
+        "1977-03-07",
+        "2333-12-31",
+        "407300237-5390682354",
+        instructions,
+    )
+
+    assert mock.id == 77
+    assert mock.nome_do_produto == "Never Mind The Bollocks"
+    assert mock.nome_da_empresa == "S Pistols"
+    assert mock.data_de_fabricacao == "1977-03-07"
+    assert mock.data_de_validade == "2333-12-31"
+    assert mock.numero_de_serie == "407300237-5390682354"
+    assert mock.instrucoes_de_armazenamento == instructions
